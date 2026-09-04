a8c9fc130fa19c773709f4093b534619e4a44130
shubhamjain-develops <shubhamjainiiitb@gmail.com>
2026-09-04 17:27:03 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        var i = 0;
+        var j = heights.Length - 1;
+        var max_area = 0;
+        var h = heights;
+        while(i<j){
+            var area = 0;
+            if(h[i]<h[j]){
+                area = h[i] * (j-i);
+            }
+            else{
+                area = h[j] * (j-i);
+            }
+            if(max_area<area) max_area = area;
+            if(h[i]<=h[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+
+        }
+        return max_area;
+    }
+}
