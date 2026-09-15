b4c282ffa023a3e59c0afbd52e6fa878006afca0
shubhamjain-develops <shubhamjainiiitb@gmail.com>
2026-09-15 20:10:29 +0530

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public void ReorderList(ListNode head) {
+        head = Rec(head, head.next);
+    }
+
+    private ListNode Rec(ListNode root, ListNode cur) {
+        if (cur == null) {
+            return root;
+        }
+
+        root = Rec(root, cur.next);
+        if (root == null) {
+            return null;
+        }
+
+        ListNode tmp = null;
+        if (root == cur || root.next == cur) {
+            cur.next = null;
+        } else {
+            tmp = root.next;
+            root.next = cur;
+            cur.next = tmp;
+        }
+
+        return tmp;
+    }
+}
